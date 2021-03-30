ea0b2c1e842f8749a742cd03cf65d35ab88b2ddf
flores_luis_m@outlook.com <luis@healthessentialtechnologies.com>
2021-03-29 23:36:12 -0400

    Removing test doubles

--- TestingSwift/TestingSwiftTests/TestDouble.swift
+++ TestingSwift/TestingSwiftTests/TestDouble.swift
@@ -29,58 +29,5 @@ class TestDouble: XCTestCase {
             // Put the code you want to measure the time of here.
         }
     }
-    
-    func test_User_Cant_Buy_Unreleased_App() {
-        struct UnreleaseAppStub: AppProtocol {
-            var price: Decimal = 0
-            var minimumAge: Int = 0
-            var isReleased: Bool = false
-            
-            func canBePurchased(by user: UserProtocol) -> Bool {
-                return false
-            }
-        }
-        
-        // Given
-        var sut = User(funds: 100, age: 21, apps: [])
-        let app = UnreleaseAppStub()
-        
-        type(of: app).printTerms()
-        
-        // When
-        let wasBought = sut.buy(app: app)
-        
-        //Then
-        XCTAssertFalse(wasBought)
-        
-    }
-    
-    func test_Viewing_House_Adds_One_To_Viewings() {
-        let house = NewHouse()
-        let startViewings = house.numberOfViewings
-        house.conductViewing()
-        XCTAssertEqual(house.numberOfViewings, startViewings + 1)
-    }
-    
-    func test_Buyer_Viewing_House_Adds_One_To_Viewing() {
-        class NewHouseMock: NewHouseProtocol {
-            var numberOfViewings: Int = 0
-            
-            func conductViewing() {
-                numberOfViewings += 1
-            }
-        }
-        let mock = NewHouseMock()
-        let sut = Buyer()
-        let startViewings = mock.numberOfViewings
-        sut.view(mock)
-        XCTAssertEqual(mock.numberOfViewings, startViewings + 1)
-    }
-    
-    func test_Unplugged_Device_Shows_Down() {
-        let sut = PowerMonitor(device: DeviceMock(testBatteryState: .unplugged))
-        let message = sut.getStatus()
-        XCTAssertEqual(message, "Power is down")
-    }
 
 }
